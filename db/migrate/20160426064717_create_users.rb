d717a358755e58e3f4b1a215d008ca45c552b6ad
Konno <dra.mixx.nico@gmail.com>
2016-04-26 16:04:38 +0900

    [add] sign up and user detail

--- db/migrate/20160426064717_create_users.rb
+++ db/migrate/20160426064717_create_users.rb
@@ -3,11 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password_digist
+      t.string :password_digest
 
       t.timestamps null: false
-
-      t.index :email, unique:true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426060242) do
+ActiveRecord::Schema.define(version: 20160426064717) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digist"
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
