cbc3d3a94629abdcb7993f124883bbbd3ad4a33f
Konno <dra.mixx.nico@gmail.com>
2016-05-04 21:26:30 +0900

    [add] login and userInfo edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,42 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
-    def show
-        @user = User.find(params[:id])
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Oreore Twitter!"
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
 
-    def new
-        @user = User.new
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'ユーザ情報を更新しました'
+    else
+      render :edit
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Welcome to the Oreore Twitter!"
-            redirect_to @user
-        else
-            render 'new'
-        end
-    end
+  private
 
-    private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
